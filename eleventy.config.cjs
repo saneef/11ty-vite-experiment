e54feeccac393ea6ec28a987f48ca8ae2785d5f4
Saneef Ansari <hello@saneef.com>
2023-11-19 02:01:28 +0530

    Adds lightningcss

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,11 +1,23 @@
 // @ts-check
 const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+const browserslist = require("browserslist");
+const { browserslistToTargets } = require("lightningcss");
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/assets");
 	eleventyConfig.addPlugin(EleventyVitePlugin, {
-		viteOptions: {},
+		viteOptions: {
+			css: {
+				transformer: "lightningcss",
+				lightningcss: {
+					targets: browserslistToTargets(browserslist(">= 0.25%")),
+				},
+			},
+			build: {
+				cssMinify: "lightningcss",
+			},
+		},
 	});
 
 	return {
